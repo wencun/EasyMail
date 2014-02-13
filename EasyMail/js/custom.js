f686ebc6b42972903bef564ba6e97a8142daf8e2
wencun <wencun2011@sina.com>
2014-02-13 15:45:46 +0800

    Revert "my first"
    This reverts commit 220aca6afcd894b3f0128bbcc0253797d60b06ab.

--- EasyMail/js/custom.js
+++
@@ -1,8 +0,0 @@
-$(document).bind("mobileinit", function(){
-	$.mobile.allowCrossDomainPages = true;
-    $.mobile.page.prototype.options.addBackBtn = true;
-});
-
-$(document).bind("deviceready", function(){
-    navigator.notification.alert("PhoneGap is initialized...");
-});
